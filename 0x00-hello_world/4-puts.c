055f8d49fed32e68bbddb68907da13cfec62bbb7
kobzbreed <onajitekobim@gmail.com>
2022-09-08 17:11:16 -0700

    task 4 Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#!/bin/bash
+/*
+ * file: 4-puts.c
+ */
+#include <stdio.h>
+/**
+ * main - prints "programming is like a multilingual puzzle.
+ * return: Always 0.
+ */
+int main(void)
+{
+puts("\"programming is like a multilingual puzzle");
+return (0);
+}
