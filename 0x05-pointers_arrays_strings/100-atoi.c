8d4ad10ed7a52ea7eaf900a5e814e2e4ce303524
kobzbreed <onajitekobim@gmail.com>
2022-09-19 13:35:56 -0700

    task 12

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,26 @@
+
+/**
+ * _atio -  convert a string to an integer
+ * @s: the sting to convert to integer
+ * Description:  convert a string of numbers to an integer
+ * Return: integer gotten from string
+ */
+int _atoi(char *s)
+{
+	int i, n, sign;
+
+	/* skip any character that is not a number*/
+	for (i = 0; s[i] < '0' || s[i] > '9'; i++)
+		;
+	/* go back 1 char and check the sign*/
+	if(i > 0)
+		s--;
+	sign = (s[i] == '-') ? -1 : 1;
+
+	/*skip sign */
+	if (s[i] == '+' || s[i] == '-')
+		i++;
+	for (n = 0; s[i] >= '0' && s[i] <= '9'; i++)
+		n = 10 * n + (s[i] - '0');
+	return (sign * n);
+}
