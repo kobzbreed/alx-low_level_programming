3188f9fa449b25f0b5fd5a393581210b80f30b02
kobzbreed <onajitekobim@gmail.com>
2022-09-19 13:42:43 -0700

    task 11

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,26 +1,46 @@
+#include "main.h"
 
 /**
- * _atio -  convert a string to an integer
- * @s: the sting to convert to integer
- * Description:  convert a string of numbers to an integer
- * Return: integer gotten from string
+ * _atoi - converts a string to an integer
+ * @s: string to be converted
+ *
+ * Return: the int converted from the string
  */
 int _atoi(char *s)
 {
-	int i, n, sign;
+	int i, d, n, len, f, digit;
 
-	/* skip any character that is not a number*/
-	for (i = 0; s[i] < '0' || s[i] > '9'; i++)
-		;
-	/* go back 1 char and check the sign*/
-	if(i > 0)
-		s--;
-	sign = (s[i] == '-') ? -1 : 1;
+	i = 0;
+	d = 0;
+	n = 0;
+	len = 0;
+	f = 0;
+	digit = 0;
 
-	/*skip sign */
-	if (s[i] == '+' || s[i] == '-')
+	while (s[len] != '\0')
+		len++;
+
+	while (i < len && f == 0)
+	{
+		if (s[i] == '-')
+			++d;
+
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			digit = s[i] - '0';
+			if (d % 2)
+				digit = -digit;
+			n = n * 10 + digit;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+			f = 0;
+		}
 		i++;
-	for (n = 0; s[i] >= '0' && s[i] <= '9'; i++)
-		n = 10 * n + (s[i] - '0');
-	return (sign * n);
+	}
+
+	if (f == 0)
+		return (0);
+
+	return (n);
 }
