e38ef244690d15dbba7b87044ce853a1618a3007
kobzbreed <jitekobimowe10@gmail.com>
2022-12-15 23:53:43 -0800

    python tests

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,54 @@
+/**
+ * add - adds input integers
+ * @m: - first input
+ * @n: - second input
+ * Return: returns output of the function
+ */
+int add(int m, int n)
+{
+	return (m + n);
+}
+
+/**
+ * sub - substract input integers
+ * @m: - first input
+ * @n: - second input
+ * Return: returns output of the function
+ */
+int sub(int m, int n)
+{
+	return (m - n);
+}
+
+/**
+ * mul - multiplies input integers
+ * @m: - first input
+ * @n: - second input
+ * Return: returns output value
+ */
+int mul(int m, int n)
+{
+	return (m*n);
+}
+
+/**
+ * div - divides input integers
+ * @m: - first input
+ * @n: - second input
+ * Return: returns dividend as output
+ */
+int div(int m, int n)
+{
+	return (m/n);
+}
+
+/**
+ * mod - performs modulus operation on input integers
+ * @m: - first input
+ * @n: - second input
+ * Return: returns remainder as output
+ */
+int mod(int m, int n)
+{
+	return(m%n);
+}
